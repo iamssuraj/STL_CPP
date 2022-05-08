05b13da846f6287a664526f1af2748720fc61bac
Suraj Sanganbhatla <ssdmp123@gmail.com>
2022-05-08 14:15:48 +0530

    maps

--- 08_Map.cpp
+++ 08_Map.cpp
@@ -3,10 +3,41 @@ using namespace std;
 
 signed main()
 {
-    map<int, string> m;
-    m[44] = "Suraj";
-    m.insert({70, "Elon"});
-    for (auto i : m)
-        cout<< i.first << " --- " << i.second << endl;
+    // map<int, string> m;
+    // m[44] = "Suraj";
+    // m.insert({70, "Elon"});
+    // for (auto i : m)
+    //     cout<< i.first << " --- " << i.second << endl;
+
+    int n;
+    cin >> n;
+    int a[n];
+    map<int, int> m;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        m[a[i]]++;
+    }
+
+    // Method - 1
+    // map<int,int> :: iterator it;
+    // for(it=m.begin();it!=m.end();it++)
+    // {
+    //      cout<<it->first<<" "<<it->second<<endl;
+    // }
+
+    
+    // Method-2
+    //  for(auto it=m.begin();it!=m.end();it++)
+    //  {
+    //       cout<<it->first<<" "<<it->second<<endl;
+    //  }
+
+
+    //Method-3
+    for (auto it : m)
+    {
+        cout << it.first << " " << it.second << endl;
+    }
     return 0;
 }
