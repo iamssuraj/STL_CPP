215bebfba9b6e8b5b3952f66fc9d945fb65fd591
Suraj Sanganbhatla <suraj.sanganbhatla@gmail.com>
2022-06-16 19:36:53 +0530

    um

--- 11_unordered_map.cpp
+++ 11_unordered_map.cpp
@@ -4,22 +4,32 @@ using namespace std;
 
 signed main()
 {
-    unordered_map<int, int> um; // no key, no map
+    // unordered_map<int, int> um; // no key, no map
 
     // Insert elements
 
     // method-1
-    um.insert(make_pair(1, 5));
-    um.insert(make_pair(2, 6));
-    um.insert(make_pair(3, 7));
+    // um.insert(make_pair(1, 5));
+    // um.insert(make_pair(2, 6));
+    // um.insert(make_pair(3, 7));
 
-    // method-2
-    um.insert({5, 8});
-    um.insert({6, 9});
-    um.insert({9, 10});
+    // // method-2
+    // um.insert({5, 8});
+    // um.insert({6, 9});
+    // um.insert({9, 10});
 
+    unordered_map<string, int> um;
+    // use of [] operator :
+    um["Suraj"] = 44;
+    um["Zallen"] = 73;
+    um["Bill"] = 71;
     for (auto it : um)
         cout << it.first << "  " << it.second << endl;
+    string search = "Zallen";
+    if(um.find(search)==um.end())
+        cout<<"Not Found"<<endl;
+    else
+        cout<<"found"<<endl;
 
     return 0;
 }
