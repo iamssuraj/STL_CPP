e17f997c9769a05e8effceba03a06167f0a9c81f
Suraj Sanganbhatla <ssdmp123@gmail.com>
2022-06-16 16:07:44 +0530

    umap

---
+++ 10_substr.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+
+signed main()
+{
+    // string substr (initial position, length of substring) const;
+    string s = "Hey I am Suraj";
+    cout << s.substr(9, 5) << endl;
+    // prints substring from 9th index of length 5
+    return 0;
+}
